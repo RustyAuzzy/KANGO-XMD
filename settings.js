862caa5d2f2b521c9f7bebd07808b516ddc25455
RustyAuzzy <154899786+RustyAuzzy@users.noreply.github.com>
2025-05-20 10:32:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~NjFlgKxK#Xj7h36pmP5aYFHo-3a6W_wtY1P6zDviwhT6AFWh2Q6k' 
 //Enter your KANGO-XMD session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -24,7 +24,7 @@ global.botname = process.env.BOT_NAME || 'KANGO-XMD'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '2540100941349' 
 
 //--------------------[ SUDO ]--------------------------//
 
